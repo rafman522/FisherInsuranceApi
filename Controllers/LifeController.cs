2cc8c8d34474bc5e62458514b26acf44851cc03b
Raphael Goldfarb <rafman522@Raphaels-MacBook-Pro.local>
2017-02-02 19:49:12 -0500

    Yikes late onmy initial commit

---
+++ Controllers/LifeController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+
+[RouteAttribute("api/life/quotes")]
+
+public class LifeController : Controller
+
+{
+// POST api/auto/quotes
+
+    [HttpPost]
+    public IActionResult Post([FromBodyAttribute]string value)
+    {
+        return Created("", value);
+    }
+
+// GET api/auto/quotes/5
+    [HttpGetAttribute("{id}")]
+    public IActionResult Get(int id)
+    {
+        return Ok("The id is: " + id);
+    }
+
+// PUT api/auto/quotes/id
+    [HttpPut("{id}")]
+    public IActionResult Put(int id, [FromBodyAttribute]string value)
+    {
+        return NoContent();
+    }
+
+// DELETE api/auto/quotes/id
+
+    [HttpDeleteAttribute("{id}")]
+    public IActionResult Delete(int id)
+    {
+        return Delete(id);
+    }
+
+}
