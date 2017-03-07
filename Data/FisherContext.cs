a9d61b722cb9978c17f9850c8cb44378956ea7ab
Raphael Goldfarb <rafman522@Raphaels-MacBook-Pro.local>
2017-03-06 21:48:17 -0500

    fisher context updated

---
+++ Data/FisherContext.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+
+using FisherInsuranceApi.Models;
+
+
+namespace FisherInsuranceApi.Data
+
+{
+
+    public class FisherContext : DbContext
+    {
+        public DbSet<Claim> Claims {get;set;}
+        protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder)
+        {
+            string connection = "User ID=fisher-user; Password=fisher;Host=localhost;port=5432;Database=fisher-insurance;Pooling=true;";
+
+            optionsBuilder.UseNpgsql(connection);
+        }
+
+    }
+
+    
+}
