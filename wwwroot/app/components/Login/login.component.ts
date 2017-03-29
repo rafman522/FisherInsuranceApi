be038e36ec3fcaf918def2c340f3928d13ae00ea
Raphael Goldfarb <rafman522@Raphaels-MacBook-Pro.local>
2017-03-29 18:59:57 -0400

    fixed baby lab 7 complete

--- wwwroot/app/components/Login/login.component.ts
+++ wwwroot/app/components/Login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AuthService } from './app/auth.service';
+import { AuthService } from '../../auth.service';
 
 @Component({
     selector: 'login',
