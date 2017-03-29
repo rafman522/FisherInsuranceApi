7b84e0ccee228a40002c7bd974fbb45464dca4dd
Raphael Goldfarb <rafman522@Raphaels-MacBook-Pro.local>
2017-03-29 01:23:38 -0400

    JWT Provider

--- wwwroot/Security/JwtProvider.cs
+++ wwwroot/Security/JwtProvider.cs
@@ -13,4 +13,26 @@ using System.Threading.Tasks;
 namespace FisherInsuranceApi.Security
 {
     public class JwtProvider
+    {
+        private readonly RequestDelegate _next;
+
+        public JwtProvider(RequestDelegate next)
+        {
+            _next = next;
+        }
+        
+        public Task Invoke(HttpContext httpContext)
+        {
+            return _next(httpContext);
+        }
+
+    }
+    
+    public static class JwtProviderExtensions
+    {
+        public static IApplicationBuilder UseJwtProvider(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<JwtProvider>();
+        }
+    }
 }
