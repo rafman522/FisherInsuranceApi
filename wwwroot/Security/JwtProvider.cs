7fd8c7335be448f88816658ed0ba4092410ab6e5
Raphael Goldfarb <rafman522@Raphaels-MacBook-Pro.local>
2017-03-29 11:31:09 -0400

    jwtprovider finisher

--- wwwroot/Security/JwtProvider.cs
+++ wwwroot/Security/JwtProvider.cs
@@ -58,8 +58,67 @@ namespace FisherInsuranceApi.Security
             }
         }
 
-    }
     
+    
+        private async Task CreateToken(HttpContext httpContext){
+            try
+            {
+                string username = httpContext.Request.Form["username"];
+                string password = httpContext.Request.Form["password"];
+
+                var user = await UserManager.FindByNameAsync(username);
+
+                if (user == null && username.Contains("@"))
+                {
+                    user = await UserManager.FindByEmailAsync(username);
+                }
+
+                var success = user != null && await UserManager.CheckPasswordAsync(user, password);
+
+                if (success)
+                {
+                    DateTime now = DateTime.UtcNow;
+
+                    var claims = new[]
+                    {
+                        new Claim(JwtRegisteredClaimNames.Iss, Issuer),
+                        new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                        new Claim(JwtRegisteredClaimNames.Iat, new DateTimeOffset(now).ToUnixTimeSeconds().ToString(),
+                        ClaimValueTypes.Integer64)
+
+                    };
+                    var token = new JwtSecurityToken(
+                        claims: claims,
+                        notBefore: now,
+                        expires: now.Add(TokenExpiration),
+                        signingCredentials: SigningCredentials);
+
+                    var encodedToken = new JwtSecurityTokenHandler().WriteToken(token);
+
+                    var jwt = new
+                    {
+                        access_token = encodedToken,
+                        expiration = (int)TokenExpiration.TotalSeconds
+                    };
+
+                    httpContext.Response.ContentType = "application/json";
+
+                    await
+                    httpContext.Response.WriteAsync(JsonConvert.SerializeObject(jwt));
+
+                    return;
+                }
+            }
+            catch (Exception)
+            {
+            
+            throw;
+            }
+            httpContext.Response.StatusCode = 400;
+            await httpContext.Response.WriteAsync("Invalid username or password.");
+        }
+    }
     public static class JwtProviderExtensions
     {
         public static IApplicationBuilder UseJwtProvider(this IApplicationBuilder builder)
